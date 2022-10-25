37719ea777413aa61c0ecc2743037123bfcd7817
CHIEN TRAN MINH <tmchien@rd.local>
2022-10-25 12:46:46 +0700

    a

--- src/money.py
+++ src/money.py
@@ -7,6 +7,19 @@ DIR_PATH = os.path.dirname(THIS_PATH)
 DDIR_PATH = os.path.dirname(DIR_PATH)
 ASSET_PATH = os.path.join(DDIR_PATH, 'Assets')
 
+def check_key():
+    temp1 = st.subheader('Tính năng hiện tại đang không sử dụng')
+    temp2 = st.subheader('Nhập đúng KEY để truy cập')
+    key = st.sidebar.text_input('Key', type = 'password')
+    if st.sidebar.checkbox('Xác nhận'):
+        if key == os.environ.get('KEY'):
+            temp1.empty()
+            temp2.empty()
+            return True
+    else:
+        st.sidebar.error('Key nhập vào chưa đúng')
+        return False
+
 def read_csv(path = os.path.join(ASSET_PATH, 'names.csv')):
     df = pd.read_csv(path, sep = ',', encoding = 'utf-8')
     st.session_state.df = df.sort_values(by = ['teams'])
@@ -18,13 +31,13 @@ def get_names():
     return names, first_names
 
 def get_money_names():
-    columns = list(st.session_state.money_df.columns)
-    names = [name for name in columns if not name.startswith('giá')]
+    names = set([col[0] for col in st.session_state.money_df.columns])
+    names.remove('Chung');names.remove('Tổng tiền')
     return names
 
 def read_money_csv(path = os.path.join(ASSET_PATH, 'money.csv')):
-    money_df = pd.read_csv(path, sep = ',', encoding = 'utf-8')
-    money_df.index = range(1, money_df.shape[0] + 1)
+    money_df = pd.read_csv(path, sep = ',', encoding = 'utf-8', header = [0, 1])
+    money_df.index = range(1, len(money_df.index) + 1)
     st.session_state.money_df = money_df.sort_index()
 
 def create_table_full(names):
@@ -32,9 +45,9 @@ def create_table_full(names):
     if sorted(mn_names) != sorted(names):
         for name in names:
             if name not in mn_names:
-                st.session_state.money_df[name] = []
                 first_name = name.split(' ')[-1]
-                st.session_state.money_df[f'tiền {first_name}'] = []
+                st.session_state.money_df[first_name, 'mua'] = []
+                st.session_state.money_df[first_name, 'giá'] = []
         save_csv()
     try:
         st.session_state.table_money.empty()
@@ -45,16 +58,16 @@ def create_table_full(names):
     with st.session_state.table_money.container():
         st.dataframe(st.session_state.money_df, use_container_width = True)
 
-def create_table_only(name, id):
+def create_table_only(name):
     first_name = name.split(' ')[-1]
-    st.session_state.money_df_only = st.session_state.money_df[[name, f'tiền {first_name}']]
+    st.session_state.money_df_only = st.session_state.money_df[[first_name]]
     try:
-        st.session_state.table_only[id].empty()
+        st.session_state.table_only.empty()
     except:
         pass
     read_csv()
-    st.session_state.table_only[id] = st.empty()
-    with st.session_state.table_only[id].container():
+    st.session_state.table_only = st.empty()
+    with st.session_state.table_only.container():
         st.dataframe(st.session_state.money_df_only, use_container_width = True)
 
 def save_csv(path = os.path.join(ASSET_PATH, 'money.csv')):
@@ -64,42 +77,37 @@ def init():
     read_csv()
     read_money_csv()
 
-def add_more(names):
+def add_more(name, thing, price):
     if thing != '' and price != '':
         first_name = name.split(' ')[-1]
-        df_only = st.session_state.money_df[[name, f'tiền {first_name}']]
+        df_only = st.session_state.money_df[first_name]
         df_only.loc[len(df_only.index) + 1] = [thing, price]
-        st.session_state.money_df[[name, f'tiền {first_name}']] = df_only
+        print(df_only.head())
+        st.session_state.money_df[first_name]['mua'] = df_only
+        print(df_only.values.T)
+        print(st.session_state.money_df[first_name].head())
+        st.session_state.money_df.fillna('Chưa có', inplace = True)
         save_csv()
-        create_table_full(get_names()[0])
     else:
         st.warning('Không được để trống các trường thông tin', icon = '⚠️')
 
 def main():
-    init()
-    names, first_names = get_names()
-    names = list(names)
-    
-    name = st.selectbox(
-        'Bạn muốn thêm vào danh sách của ai?',
-        tuple(names)
-    )
-    thing = st.text_input('Mua gì:', key = f't1').strip()
-    price = st.text_input('Giá:', key = f'p1').strip()
-    btn = st.button('Xác nhận', key = f'b1', on_click = add_more, args = (names,))
-    create_table_full(names)
-    # st.session_state.table_only = [st.empty()] * (len(names) + 1)
-    # temp = ['Tổng', 'Phí chung'] + first_names
-    # ls_tabs = st.tabs(temp)
-    # for idx, tab in enumerate(ls_tabs):
-    #     with tab:
-    #         if idx == 0:
-    #             create_table_full(names)
-    #             continue
-    #         elif idx == 1:
-    #             create_table_only('Phí chung', 0)
-    #             continue
-    #         create_table_only(names[idx - 2], idx - 1)
-    
+    if check_key():
+        init()
+        names, first_names = get_names()
+        names = list(names)
+        
+        name = st.selectbox(
+            'Lựa chọn điểm xem phù hợp',
+            ('Tổng tiền', 'Chung') + tuple(names)
+        )
+        if name == 'Tổng tiền':
+            create_table_full(first_names)
+        else:
+            create_table_only(name)
+            thing = st.text_input('Mua gì:', key = f't1').strip()
+            price = st.text_input('Giá:', key = f'p1').strip()
+            btn = st.button('Xác nhận', key = f'b1', on_click = add_more, args = (name, thing, price)) 
+        
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,22 +1,54 @@
-import pandas as pd
-import os
+# from __future__ import print_function
 
-THIS_PATH = os.path.abspath(__file__)
-DIR_PATH = os.path.dirname(THIS_PATH)
-DDIR_PATH = os.path.dirname(DIR_PATH)
-ASSET_PATH = os.path.join(DDIR_PATH, 'Assets')
+import os.path
 
-def read_money_csv(path = os.path.join(ASSET_PATH, 'money.csv')):
-    money_df = pd.read_csv(path, sep = ',', encoding = 'utf-8')
-    money_df.index = range(1, money_df.shape[0] + 1)
-    money_df = money_df.sort_index()
-    return money_df
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from sympy import content
 
-df = read_money_csv('E:\st-web\Assets\money.csv')
-df_only = df[['Trần Minh Chiến', 'tiền Chiến']]
+from pprint import pprint
+import json
+from googleapiclient import discovery
 
-df_only.loc[len(df_only.index) + 1] = [1, 1]
-df[['Trần Minh Chiến', 'tiền Chiến']] = df_only
+# TODO: Change placeholder below to generate authentication credentials. See
+# https://developers.google.com/sheets/quickstart/python#step_3_set_up_the_sample
+#
+# Authorize using one of the following scopes:
+#     'https://www.googleapis.com/auth/drive'
+#     'https://www.googleapis.com/auth/drive.file'
+#     'https://www.googleapis.com/auth/spreadsheets'
 
-print(df_only.head())
-print(df.head())
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+with open('E:\st-web\data.json', encoding = 'utf-8') as f:
+    contents = json.loads(f.read())
+
+def get_cred():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'E:\st-web\Assets\credentials_sheet.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+    return creds
+
+service = discovery.build('sheets', 'v4', credentials=get_cred())
+
+spreadsheet_body = contents
+
+request = service.spreadsheets().create(body=spreadsheet_body)
+response = request.execute()
+
+# TODO: Change code below to process the `response` dict:
+pprint(response)
